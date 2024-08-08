d0cbf914c5b67cc62b68a4de4d4b36bc4cf01a93
HAMZA ABUABADA <abadahhamza@gmail.com>
2024-08-08 16:57:26 +0300

    feat(Wallet): check wallet balance on pos Invoice (#3)
    * feat(Wallet): edit check customer wallet with fetch pos invoice
    
    * feat(POS Invoice): check customer wallet on submit pos invoice
    
    * feat(init): update version
    
    * feat(Wallet): edit get customer wallet with open pos invoice
    
    * feat(Pos Invoice): edit throw message
    
    * feat(POS Payment): edit doc variable name

--- wallete/wallete/doctype/wallet/wallet.py
+++ wallete/wallete/doctype/wallet/wallet.py
@@ -1,24 +1,63 @@
 # Copyright (c) 2024, Hamza Abuabada and contributors
 # For license information, please see license.txt
 import frappe
-# import frappe
 from frappe.model.document import Document
 from erpnext.accounts.utils import get_balance_on
 
 
 class Wallet(Document):
-	pass
+    pass
 
 
 @frappe.whitelist()
-def get_customer_wallet(customer):
-	try:
-		customer_wallet = frappe.get_doc("Wallet", {'customer': customer})
-		return get_balance_on(
-			account=customer_wallet.account,
-			party_type="Customer",
-			party=customer_wallet.customer
-		)
-	except frappe.DoesNotExistError:
-		return 0.0
+def get_customer_wallet(customer, exclude_invoice):
+    try:
+        customer_wallet_doc = frappe.get_doc("Wallet", {'customer': customer})
+        customer_wallet_amount = get_balance_on(
+            account=customer_wallet_doc.account,
+            party_type="Customer",
+            party=customer_wallet_doc.customer
+        )
 
+        pos_invoices = get_customer_open_pos_invoice(
+            customer=customer,
+            exclude_invoice=exclude_invoice
+        )
+
+        open_pos_wallet_amount = 0.0
+        if len(pos_invoices) != 0:
+            for pos_invoice in pos_invoices:
+                wallet_amount_from_payments = get_wallet_amount_from_payments(pos_invoice.payments)
+                open_pos_wallet_amount = open_pos_wallet_amount + wallet_amount_from_payments
+
+        return customer_wallet_amount - open_pos_wallet_amount
+    except frappe.DoesNotExistError:
+        return 0.0
+
+
+def get_wallet_amount_from_payments(payments):
+    wallet_amount = 0.0
+    for payment in payments:
+        payment_doc = frappe.get_doc("Mode of Payment", payment.mode_of_payment)
+        if payment_doc.is_wallet_payment and payment.amount > 0.0:
+            wallet_amount = wallet_amount + payment.amount
+
+    return wallet_amount
+
+
+def get_customer_open_pos_invoice(customer, exclude_invoice):
+    data = frappe.db.sql(
+        """
+    select
+        name, timestamp(posting_date, posting_time) as "timestamp"
+    from
+        `tabPOS Invoice`
+    where
+        docstatus = 1 and ifnull(consolidated_invoice,'') = '' and customer = %s and name != %s
+    """,
+        (customer, exclude_invoice),
+        as_dict=1,
+    )
+    data = [frappe.get_doc("POS Invoice", d.name).as_dict() for d in data]
+
+    return data
