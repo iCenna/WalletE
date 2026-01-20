49df560f9985b67712add4d869edb0dff321027c
Mohanad Alkrunz <malkrunz@icenna.com>
2026-01-20 10:03:27 +0200

    Fix Customer Wallet Issue

--- wallete/doc_events/pos_invoice.py
+++ wallete/doc_events/pos_invoice.py
@@ -9,6 +9,6 @@ from wallete.wallete.doctype.wallet.wallet import (
 def override_on_submit(doc, method):
 	wallet_amount = get_wallet_amount_from_payments(doc.payments)
 	customer_wallet = get_customer_wallet_balance(doc.customer, doc.name)
-
+	customer_wallet = abs(customer_wallet)
 	if wallet_amount > customer_wallet:
 		throw(_(f"Customer Wallet Balance Must grater than or equal paid from amount {wallet_amount} {customer_wallet}"))
