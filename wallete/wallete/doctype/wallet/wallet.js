89faf2f65dffd6f883da55325124fc75b863f0a1
Mohanad Alkrunz <malkrunz@icenna.com>
2026-01-25 14:42:46 +0200

    Show Wallet Balance

--- wallete/wallete/doctype/wallet/wallet.js
+++ wallete/wallete/doctype/wallet/wallet.js
@@ -13,4 +13,34 @@ frappe.ui.form.on('Wallet', {
 			};
 		});
 	},
+	
+	refresh: function(frm) {
+		if (frm.doc.customer) {
+			frm.trigger('update_balance');
+		}
+	},
+	
+	customer: function(frm) {
+		if (frm.doc.customer) {
+			frm.trigger('update_balance');
+		} else {
+			frm.set_value('current_balance', 0);
+		}
+	},
+	
+	update_balance: function(frm) {
+		if (frm.doc.customer) {
+			frappe.call({
+				method: 'wallete.wallete.doctype.wallet.wallet.get_customer_wallet_balance',
+				args: {
+					customer: frm.doc.customer
+				},
+				callback: function(r) {
+					if (r.message !== undefined) {
+						frm.set_value('current_balance', r.message);
+					}
+				}
+			});
+		}
+	}
 });
